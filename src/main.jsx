0f678c49f79647d1e582b2734e086db79ba23aec
Esteban Salazar Zapata <esteban.salazar@Esteban-Salazar-MacBook-Pro.local>
2022-11-02 10:04:49 -0500

    feat(jsx): study about JSX sintax

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,38 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import './index.css';
 
+const course = 'React';
+
+const content = (
+	<>
+		<h2 style={{ color: 'blue', fontSize: '1.5rem' }}>Content</h2>
+		<p>Here you can find all the content of the course</p>
+		<form>
+			<label htmlFor='text'>Text</label>
+			<input type='text' id='text' defaultValue='Default' />
+		</form>
+	</>
+);
+
 const app = (
 	<div>
-		<h1>Esteban Salazar</h1>
-		<img src='/vite.svg' />
+		<h1 className='title' style={{}}>
+			{course} Course
+		</h1>
+		<p>Welcome to {course} course</p>
+		<p>This course have 25 sections</p>
+		<div>{content}</div>
+		<button onClick={() => alert('Access')}>Access</button>
 	</div>
 );
 const container = document.getElementById('root');
 
-ReactDOM.render(app, container);
+// This is the way of React 17
+// ReactDOM.render(app, container);
+
+const root = createRoot(container);
+root.render(app);
 
 // import React from 'react'
 // import ReactDOM from 'react-dom/client'
