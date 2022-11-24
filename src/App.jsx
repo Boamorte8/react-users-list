427fb684e4c81353afda7b076209443c3acaaa2c
Esteban Salazar <esteban.salazar@gorillalogic.com>
2022-11-23 20:38:33 -0500

    feat(project): :sparkles: implement custom events example

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,42 @@
 import { useEffect, useState } from 'react';
-import { createPortal } from 'react-dom';
 
-const Child = () => {
-	const [portal, setPortal] = useState(null);
+const Listener = () => {
+	const [message, setMessage] = useState();
 
 	useEffect(() => {
-		setPortal(
-			createPortal(<h1>This is a portal</h1>, document.getElementById('portal'))
-		);
+		const handleMessage = ev => {
+			setMessage(ev.detail.message);
+		};
+
+		document.addEventListener('showMessage', handleMessage);
+		return () => document.removeEventListener('showMessage', handleMessage);
 	}, []);
 
-	return portal;
-
-	// This will not work properly
-	// return createPortal(
-	// 	<h1>This is a portal</h1>,
-	// 	document.getElementById('portal')
-	// );
+	return <h1>{message || 'Waiting message...'}</h1>;
 };
 
-const HeadPortal = () =>
-	createPortal(
-		<>
-			<title>Boamorte</title>
-			<meta name='description' content='Boamorte page'></meta>
-		</>,
-		document.head
-	);
+const Emitter = () => {
+	// detail is not optional name, javascript not allow other name prop
+	return (
+		<button
+			onClick={() => {
+				const event = new CustomEvent('showMessage', {
+					detail: { message: Math.random() }
+				});
 
-const PortalA = () =>
-	createPortal(<h1>Portal A</h1>, document.getElementById('portal'));
-
-const PortalB = () =>
-	createPortal(<h1>Portal B</h1>, document.getElementById('portal'));
-
-const PortalC = () => {
-	return createPortal(
-		<button>Portal C</button>,
-		document.getElementById('portal')
+				document.dispatchEvent(event);
+			}}
+		>
+			Emit
+		</button>
 	);
 };
 
 const App = () => {
-	const [showA, setShowA] = useState(true);
-	const [showB, setShowB] = useState(true);
 	return (
 		<>
-			<HeadPortal />
-			{showA && <PortalA />}
-			{showB && <PortalB />}
-			<button onClick={() => setShowA(!showA)}>Show/Hide A</button>
-			<button onClick={() => setShowB(!showB)}>Show/Hide B</button>
-
-			<div onClick={() => console.log('Click on div')}>
-				<PortalC />
-			</div>
-
-			{/* <h1>This is my App</h1>
-			<div id='portal'></div> */}
-			<Child />
+			<Listener />
+			<Emitter />
 		</>
 	);
 };
